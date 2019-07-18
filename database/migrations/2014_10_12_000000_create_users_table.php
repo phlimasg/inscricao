fc6f5d59e6f01d8d05c85a2773df5d3b2ebfac7d
phlimasg <raphaelpc_@hotmail.com>
2019-07-18 17:20:38 -0300

    enable fk

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,6 +13,7 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::enableForeignKeyConstraints();
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
